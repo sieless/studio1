b8c7589c4b72b6d6fd84e91126f5c44425fae37f
Titus Kipkirui <titwzmaihya@gmail.com>
2025-09-25 17:04:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8585842935-1485a",
+  "appId": "1:183517980169:web:7a35cafdec76d857553ad8",
+  "apiKey": "AIzaSyDXDbd7n6pATyZnvEosKMWseWA8fg1mnU8",
+  "authDomain": "studio-8585842935-1485a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "183517980169"
+};
